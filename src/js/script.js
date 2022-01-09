161d839e525559b497386cd4fa0cef1708a26134
Yan <yan.bocharovv@gmail.com>
2022-01-09 19:20:10 +0300

    Сделал слайдер

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1000,
-        adaptiveHeight: true,
+        adaptiveHeight: false,
         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"</button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"</button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"</button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false,
+                }
+            }
+        ]
     });
 });
                   
